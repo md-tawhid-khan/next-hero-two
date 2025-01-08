1b1f9d546e23e1ad60d34e63035c5e8ca34cd930
md1300 <md.tawhid.khan1998@gmail.com>
2025-01-08 11:25:33 +0600

    loading with api data

---
+++ src/app/posts/[id]/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            <h6>post details is loading ..............</h6>
+        </div>
+    );
+};
+
+export default loading;
---
+++ src/app/posts/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            <h4>post data is loading ..............</h4>
+        </div>
+    );
+};
+
+export default loading;
